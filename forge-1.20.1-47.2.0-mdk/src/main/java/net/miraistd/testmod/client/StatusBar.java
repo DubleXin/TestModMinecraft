3328303ba0098d28b498684fed166c5fca80a5e2
Kirserd <87417121+Kirserd@users.noreply.github.com>
2023-12-28 03:50:34 +0100

    Status bar test

---
+++ forge-1.20.1-47.2.0-mdk/src/main/java/net/miraistd/testmod/client/StatusBar.java
@@ -0,0 +1,21 @@
+package net.miraistd.testmod.client;
+
+import net.miraistd.testmod.TestMod;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+
+public class StatusBar{
+    public static final ResourceLocation BACKGROUND = new ResourceLocation(TestMod.MOD_ID, "resources/assets/gui/statusbox.png");
+    public static final IGuiOverlay STATUS_BOX = ((forgeGui, guiGraphics, v, i, i1) -> {
+
+        int x = guiGraphics.guiWidth() /2;
+        int y = guiGraphics.guiHeight();
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0f,1.0f,1.0f,1.0f);
+        RenderSystem.setShaderTexture(0, BACKGROUND);
+    });
+}
