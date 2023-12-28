96139d076f89071c8a0626304370111c1b194ac8
DubleXin <progames720.2@gmail.com>
2023-12-28 03:58:42 +0100

    move_directory.exe

---
+++ forge-1.20.1-47.2.0-mdk/src/main/java/net/miraistd/testmod/QuestionMarkBlock/QuestionMarkBlock.java
@@ -0,0 +1,30 @@
+package net.miraistd.testmod.QuestionMarkBlock;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.miraistd.testmod.TestMod;
+
+import java.util.function.Supplier;
+
+public class QuestionMarkBlock {
+    public static DeferredRegister<Block> Blocks =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, TestMod.MOD_ID);
+
+    public static void register(IEventBus eventBus){
+        Blocks.register(eventBus);
+    }
+
+    private static <T extends Block>RegistryObject<T> registrBlock(String name, Supplier<T> block, CreativeModeTab tab){
+        return Blocks.register(name, block);
+    }
+    // TODO: make to the end
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab){
+        return null;
+    }
+}
--- forge-1.20.1-47.2.0-mdk/src/main/java/net/miraistd/testmod/TestMod.java
+++ forge-1.20.1-47.2.0-mdk/src/main/java/net/miraistd/testmod/TestMod.java
@@ -2,14 +2,19 @@ package net.miraistd.testmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.miraistd.testmod.Halberd.Halberd;
+import net.miraistd.testmod.QuestionMarkBlock.QuestionMarkBlock;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -29,8 +34,15 @@ public class TestMod {
 
         MinecraftForge.EVENT_BUS.register(this);
 
+        Halberd.register(modEventBus);
+        QuestionMarkBlock.register(modEventBus);
     }
 
+    private void addCreativeTab(BuildCreativeModeTabContentsEvent event){
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
+            event.accept(Halberd.HalberdItem);
+        }
+    }
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
